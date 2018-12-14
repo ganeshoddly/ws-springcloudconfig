7fb61e8a55ef1d29756b1e3d6bbe7a54bc3f5edb
Ganesh Shanmugam <ganesh.shanmugam@synapse.com>
2018-12-14 22:39:16 +0530

    initial

---
+++ git-config-server/src/test/java/com/example/gitconfigserver/GitConfigServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.gitconfigserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
